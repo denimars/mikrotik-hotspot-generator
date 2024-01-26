1632ba188357754653780874663128e7713fd512
denimarswnadi <denimarswandi@gmail.com>
2024-01-26 15:52:01 +0800

    ok

---
+++ main.py
@@ -0,0 +1,11 @@
+import routeros_api
+
+
+try:
+  connection = routeros_api.RouterOsApiPool('10.10.10.1', username='', password='', plaintext_login=True)
+  api = connection.get_api()
+  list = api.get_resource('/ip/hotspot/user')
+  list.edit()
+  connection.disconnect()
+except Exception as e:
+  print(e)

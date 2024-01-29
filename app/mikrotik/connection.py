b58f5d9e45c4ac9a7a2258d62859026c22d7292e
denimarswnadi <denimarswandi@gmail.com>
2024-01-29 16:35:29 +0800

    data fix

---
+++ app/mikrotik/connection.py
@@ -0,0 +1,14 @@
+import routeros_api
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+class MikrotikConnection():
+    def __init__(self):
+        self.mikrotik_connection = routeros_api.RouterOsApiPool(os.getenv['HOTSPOT_MIKROTIK_IP'], username=os.getenv['HOTSPOT_MIKROTIK_USER'], password=os.getenv['password'], plaintext_login=True)
+        self.mikrotik_api = self.mikrotik_connection.get_api()
+    
+    def mikrotik_api(self):
+        return self.mikrotik_api
+    
+    
